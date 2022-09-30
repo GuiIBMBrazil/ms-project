5072be8b91789ad70ca151b650d4ae4f03fcaf38
GuiIBMBrazil <Guilherme.Oliveira.Ribeiro@ibm.com>
2022-09-30 07:14:51 -0300

    criado balanceamento com o ribbon

--- hr-payroll/src/main/java/com/example/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/example/hrpayroll/resources/PaymentResource.java
@@ -2,16 +2,23 @@ package com.example.hrpayroll.resources;
 
 import com.example.hrpayroll.entities.Payment;
 import com.example.hrpayroll.services.PaymentService;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @RestController
 @RequestMapping(value = "/payments")
 public class PaymentResource {
+
+    @Autowired
+    private Environment environment;
 
     @Autowired
     private PaymentService paymentService;
